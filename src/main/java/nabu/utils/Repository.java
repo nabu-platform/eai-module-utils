8d56c10ac0daeaf23862a189fd2a8225b9bccd6a
alex <alex@nabu.be>
2016-02-06 11:16:45 +0100

    Cleanup

--- src/main/java/nabu/utils/Repository.java
+++ src/main/java/nabu/utils/Repository.java
@@ -9,32 +9,22 @@ import java.util.List;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import be.nabu.eai.repository.EAIResourceRepository;
-import be.nabu.eai.repository.api.Node;
+import be.nabu.libs.artifacts.api.Artifact;
 import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.DefinedType;
 
 @WebService
 public class Repository {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
 	@WebResult(name = "complexTypes")
 	public List<ComplexType> getComplexTypes() {
 		List<ComplexType> artifacts = new ArrayList<ComplexType>();
 		EAIResourceRepository instance = EAIResourceRepository.getInstance();
 		if (instance != null) {
-			for (Node node : instance.getNodes(DefinedType.class)) {
-				try {
-					if (node.getArtifact() instanceof ComplexType) {
-						artifacts.add((ComplexType) node.getArtifact());
-					}
-				}
-				catch (Exception e) {
-					logger.error("Could not load: " + node, e);
+			for (DefinedType type : instance.getArtifacts(DefinedType.class)) {
+				if (type instanceof ComplexType) {
+					artifacts.add((ComplexType) type);
 				}
 			}
 		}
@@ -44,9 +34,9 @@ public class Repository {
 	@WebResult(name = "complexType")
 	public ComplexType getComplexType(String id) throws IOException, ParseException {
 		EAIResourceRepository instance = EAIResourceRepository.getInstance();
-		Node node = instance.getNode(id);
-		if (node != null && node.getArtifact() instanceof ComplexType) {
-			return (ComplexType) node.getArtifact();
+		Artifact artifact = instance.resolve(id);
+		if (artifact != null && artifact instanceof ComplexType) {
+			return (ComplexType) artifact;
 		}
 		return null;
 	}
