75aed545d1465c05ad228d0a350d7913e22a717d
alex <alex@nabu.be>
2016-04-18 12:29:37 +0200

    Added some util methods

--- src/main/java/nabu/utils/Uri.java
+++ src/main/java/nabu/utils/Uri.java
@@ -1,11 +1,13 @@
 package nabu.utils;
 
 import java.net.URI;
+import java.lang.String;
 
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import be.nabu.libs.resources.URIUtils;
 import nabu.utils.types.UriComponents;
 
 @WebService
@@ -15,5 +17,14 @@ public class Uri {
 	public UriComponents toComponents(@WebParam(name = "uri") URI uri) {
 		return UriComponents.build(uri);
 	}
+
+	@WebResult(name = "uri")
+	public String encodeUri(@WebParam(name = "uri") String uri) {
+		return URIUtils.encodeURI(uri);
+	}
 	
+	@WebResult(name = "component")
+	public String encodeUriComponent(@WebParam(name = "component") String component) {
+		return URIUtils.encodeURIComponent(component);
+	}
 }
