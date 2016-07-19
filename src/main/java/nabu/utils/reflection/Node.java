4ddbc484e6c49feb6e63053232e8b957574b685c
alex <alex@nabu.be>
2016-07-19 16:58:47 +0200

    Allow retrieval of all services

--- src/main/java/nabu/utils/reflection/Node.java
+++ src/main/java/nabu/utils/reflection/Node.java
@@ -1,5 +1,7 @@
 package nabu.utils.reflection;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +11,19 @@ import javax.jws.WebService;
 
 import be.nabu.eai.repository.EAIResourceRepository;
 import be.nabu.eai.repository.api.Entry;
+import be.nabu.libs.property.api.Value;
+import be.nabu.libs.services.api.DefinedService;
 import be.nabu.libs.services.api.Service;
+import be.nabu.libs.types.TypeUtils;
 import be.nabu.libs.types.api.ComplexType;
+import be.nabu.libs.types.api.DefinedType;
+import be.nabu.libs.types.api.Element;
 import be.nabu.libs.types.api.SimpleType;
+import be.nabu.libs.types.properties.CommentProperty;
+import be.nabu.libs.types.properties.MaxOccursProperty;
 import nabu.utils.types.NodeDescription;
+import nabu.utils.types.ParameterDescription;
+import nabu.utils.types.ServiceDescription;
 
 @WebService
 public class Node {
@@ -30,6 +41,65 @@ public class Node {
 			}
 		}
 		return nodes;
+	}
+	
+	@WebResult(name = "services")
+	public List<ServiceDescription> services(@WebParam(name = "id") String id, @WebParam(name = "recursive") Boolean recursive) {
+		Entry entry = id == null 
+			? EAIResourceRepository.getInstance().getRoot()
+			: EAIResourceRepository.getInstance().getEntry(id);
+		List<ServiceDescription> nodes = new ArrayList<ServiceDescription>();
+		if (entry != null) {
+			getAllServices(entry, nodes, recursive);
+		}
+		return nodes;
+	}
+
+	private void getAllServices(Entry entry, List<ServiceDescription> nodes, Boolean recursive) {
+		getSingleDescription(nodes, entry);
+		for (Entry child : entry) {
+			getSingleDescription(nodes, child);
+			if (recursive != null && recursive) {
+				getAllServices(child, nodes, recursive);
+			}
+		}
+	}
+
+	private void getSingleDescription(List<ServiceDescription> nodes, Entry child) {
+		if (child.isNode() && Service.class.isAssignableFrom(child.getNode().getArtifactClass())) {
+			try {
+				ServiceDescription description = getServiceDescription(child);
+				nodes.add(description); 
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private ServiceDescription getServiceDescription(Entry child) throws IOException, ParseException {
+		ServiceDescription description = new ServiceDescription();
+		description.setId(child.getId());
+		description.setName(child.getName());
+		description.setType("service");
+		description.setArtifactClass(child.getNode().getArtifactClass().getName());
+		description.setLeaf(child.isLeaf());
+		DefinedService service = (DefinedService) child.getNode().getArtifact();
+		description.setInputs(toParameters(service.getServiceInterface().getInputDefinition()));
+		description.setOutputs(toParameters(service.getServiceInterface().getOutputDefinition()));
+		return description;
+	}
+	
+	private static List<ParameterDescription> toParameters(ComplexType type) {
+		List<ParameterDescription> parameters = new ArrayList<ParameterDescription>();
+		for (Element<?> element : TypeUtils.getAllChildren(type)) {
+			Value<Integer> maxOccurs = element.getProperty(MaxOccursProperty.getInstance());
+			Value<String> comment = element.getProperty(CommentProperty.getInstance());
+			parameters.add(new ParameterDescription(element.getName(), element.getType() instanceof DefinedType ? ((DefinedType) element.getType()).getId() : null, 
+				comment == null ? null : comment.getValue(),
+				maxOccurs != null && maxOccurs.getValue() != null && maxOccurs.getValue() != 1));
+		}
+		return parameters;
 	}
 
 	private NodeDescription getDescription(Entry entry, Boolean recursive) {
