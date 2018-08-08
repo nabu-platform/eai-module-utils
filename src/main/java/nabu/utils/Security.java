c85421704e62af4b41624a0319d872a6b0e705a8
Alexander Verbruggen <alex@nabu.be>
2018-08-08 13:59:00 +0200

    Added pbe encryption/decryption

--- src/main/java/nabu/utils/Security.java
+++ src/main/java/nabu/utils/Security.java
@@ -1,15 +1,24 @@
 package nabu.utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.validation.constraints.NotNull;
 
+import be.nabu.utils.io.IOUtils;
 import be.nabu.utils.security.DigestAlgorithm;
+import be.nabu.utils.security.PBEAlgorithm;
 import be.nabu.utils.security.SecurityUtils;
 
 @WebService
@@ -28,5 +37,17 @@ public class Security {
 	@WebResult(name = "valid")
 	public java.lang.Boolean validateHash(@WebParam(name = "string") java.lang.String content, @NotNull @WebParam(name = "hash") java.lang.String hash, @NotNull @WebParam(name = "algorithm") DigestAlgorithm algorithm) throws NoSuchAlgorithmException, IOException {
 		return content == null ? false : SecurityUtils.check(content, hash, algorithm);
+	}
+	
+	@WebResult(name = "encrypted")
+	public InputStream pbeEncrypt(@WebParam(name = "data") InputStream input, @WebParam(name = "password") java.lang.String password, @WebParam(name = "algorithm") PBEAlgorithm algorithm) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException {
+		byte[] bytes = IOUtils.toBytes(IOUtils.wrap(input));
+		return new ByteArrayInputStream(SecurityUtils.pbeEncrypt(bytes, password, algorithm).getBytes("ASCII"));
+	}
+	
+	@WebResult(name = "decrypted")
+	public InputStream pbeDecrypt(@WebParam(name = "encrypted") InputStream input, @WebParam(name = "password") java.lang.String password, @WebParam(name = "algorithm") PBEAlgorithm algorithm) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException {
+		byte[] bytes = IOUtils.toBytes(IOUtils.wrap(input));
+		return new ByteArrayInputStream(SecurityUtils.pbeDecrypt(new java.lang.String(bytes, "ASCII"), password, algorithm));
 	}
 }
