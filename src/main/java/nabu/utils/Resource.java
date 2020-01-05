1200801b7d3054b47c5fb2ea5ec27496ffc1a347
Alexander Verbruggen <alex@nabu.be>
2020-01-05 14:52:18 +0100

    push resource changes if they are to the repository

--- src/main/java/nabu/utils/Resource.java
+++ src/main/java/nabu/utils/Resource.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import javax.jws.WebParam;
@@ -13,14 +14,20 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import be.nabu.eai.api.Hidden;
+import be.nabu.eai.repository.EAIResourceRepository;
+import be.nabu.eai.repository.events.ResourceEvent;
+import be.nabu.eai.repository.events.ResourceEvent.ResourceState;
 import be.nabu.libs.resources.ResourceFactory;
 import be.nabu.libs.resources.ResourceReadableContainer;
 import be.nabu.libs.resources.ResourceUtils;
+import be.nabu.libs.resources.ResourceWritableContainer;
+import be.nabu.libs.resources.URIUtils;
 import be.nabu.libs.resources.api.ManageableContainer;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.ResourceContainer;
 import be.nabu.libs.resources.api.ResourceProperties;
 import be.nabu.libs.resources.api.ResourceResolver;
+import be.nabu.libs.resources.api.WritableResource;
 import be.nabu.libs.services.api.ExecutionContext;
 import be.nabu.utils.io.IOUtils;
 import be.nabu.utils.io.api.ByteBuffer;
@@ -104,9 +111,24 @@ public class Resource {
 	
 	public void write(@WebParam(name = "uri") URI uri, @WebParam(name = "stream") InputStream content) throws IOException {
 		if (uri != null || content != null) {
-			WritableContainer<ByteBuffer> writableContainer = ResourceUtils.toWritableContainer(uri, null);
+			// for optimal signalling to connected systems we need to know if the file already existed (update) or was created in the process
+			// this may require different actions from people watching the file system
+			ResourceState state = ResourceState.UPDATE;
+			be.nabu.libs.resources.api.Resource resource = ResourceFactory.getInstance().resolve(uri, null);
+			if (resource == null) {
+				resource = ResourceUtils.touch(uri, null);
+				state = ResourceState.CREATE;
+			}
+			if (resource == null) {
+				throw new FileNotFoundException("Could not find or create the resource " + ResourceUtils.cleanForLogging(uri));
+			}
+			if (!(resource instanceof WritableResource)) {
+				throw new IOException("The resource at " + ResourceUtils.cleanForLogging(uri) + " is not writable");
+			}
+			WritableContainer<ByteBuffer> writableContainer = new ResourceWritableContainer((WritableResource) resource);;
 			try {
 				IOUtils.copyBytes(IOUtils.wrap(content), writableContainer);
+				trigger(uri, state);
 			}
 			finally {
 				writableContainer.close();
@@ -122,6 +144,7 @@ public class Resource {
 				try {
 					if (parent instanceof ManageableContainer) {
 						((ManageableContainer<?>) parent).delete(resolve.getName());
+						trigger(uri, ResourceState.DELETE);
 					}
 				}
 				finally {
@@ -139,6 +162,7 @@ public class Resource {
 			if (directory instanceof Closeable) {
 				((Closeable) directory).close();
 			}
+			trigger(uri, ResourceState.CREATE);
 		}
 	}
 	
@@ -149,5 +173,24 @@ public class Resource {
 		if (newInstance instanceof ResourceResolver) {
 			ResourceFactory.getInstance().addResourceResolver((ResourceResolver) newInstance);
 		}
+	}
+	
+	private void trigger(URI uri, ResourceState state) {
+		if ("repository".equals(uri.getScheme())) {
+			try {
+				URI childUri = new URI(URIUtils.encodeURI(uri.getSchemeSpecificPart()));
+				trigger(state, childUri.getScheme(), childUri.getPath());
+			}
+			catch (URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	private void trigger(ResourceState state, java.lang.String artifactId, java.lang.String path) {
+		ResourceEvent event = new ResourceEvent();
+		event.setState(state);
+		event.setArtifactId(artifactId);
+		event.setPath(path);
+		EAIResourceRepository.getInstance().getEventDispatcher().fire(event, this);
 	}
 }
--- src/main/java/nabu/utils/Token.java
+++ src/main/java/nabu/utils/Token.java
@@ -1,17 +1,55 @@
 package nabu.utils;
 
+import java.security.Principal;
+
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.validation.constraints.NotNull;
 
 import be.nabu.eai.repository.util.SystemPrincipal;
+import be.nabu.libs.authentication.api.Device;
 import be.nabu.libs.authentication.api.WrappedToken;
+import be.nabu.libs.authentication.api.principals.DevicePrincipal;
 import be.nabu.libs.authentication.impl.BasicPrincipalImpl;
 import be.nabu.libs.authentication.impl.ImpersonateToken;
 
 @WebService
 public class Token {
 	
+	public static class DevicePrincipalImplementation implements DevicePrincipal {
+		private java.lang.String name;
+		private Device device;
+		private static final long serialVersionUID = 1L;
+
+		public DevicePrincipalImplementation() {
+			// auto construct
+		}
+		
+		public DevicePrincipalImplementation(java.lang.String name, Device device) {
+			this.name = name;
+			this.device = device;
+		}
+
+		@Override
+		public java.lang.String getName() {
+			return name;
+		}
+
+		@Override
+		public Device getDevice() {
+			return device;
+		}
+
+		public void setName(java.lang.String name) {
+			this.name = name;
+		}
+
+		public void setDevice(Device device) {
+			this.device = device;
+		}
+	}
+
 	public boolean isSystemToken(@WebParam(name = "token") be.nabu.libs.authentication.api.Token token) {
 		return token instanceof SystemPrincipal;
 	}
@@ -39,5 +77,10 @@ public class Token {
 	@WebResult(name = "token")
 	public be.nabu.libs.authentication.api.Token newBasicToken(@WebParam(name = "name") java.lang.String user, @WebParam(name = "password") java.lang.String password) {
 		return new BasicPrincipalImpl(user, password);
+	}
+	
+	@WebResult(name = "principal")
+	public Principal newDevicePrincipal(final @NotNull @WebParam(name = "name") java.lang.String name, final @NotNull @WebParam(name = "device") Device device) {
+		return new DevicePrincipalImplementation(name, device);
 	}
 }
